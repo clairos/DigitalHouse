1d7e30c032b532fa6d9a86d66fcac49586b90173
Ana Clara Brusamarello Barbosa <clarasgore@gmail.com>
2022-08-09 11:14:16 -0300

    exercicios banco de dados teste

---
+++ modulo6/funcoes de agregacao e consulta avancada/exercicios_aula_22-08-09.sql
@@ -0,0 +1,14 @@
+SELECT id_turma, turmas.semestre, turmas.ano_inicio, professores.nome AS nome_professor
+FROM turmas 
+INNER JOIN professores ON turmas.id_professor = professores.id_professor
+WHERE turmas.id_professor IN (1, 3, 15);
+
+--------------------------
+SELECT id_turma, COUNT(id_aluno) AS alunos, SUM(faltas) AS nr_faltas 
+FROM alunos_turmas
+GROUP BY id_turma
+HAVING alunos <= 5;
+
+--------------------------
+SELECT DISTINCT(alunos_turmas.id_aluno), alunos.* FROM alunos_turmas
+RIGHT JOIN alunos USING(id_aluno)
