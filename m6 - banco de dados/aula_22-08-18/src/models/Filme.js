fe9c42a4504f47f827e6387ba0a476c6d324e58f
Ana Clara Brusamarello Barbosa <clarasgore@gmail.com>
2022-08-18 11:10:29 -0300

    configuração pastas config/models

---
+++ m6 - banco de dados/aula_22-08-18/src/models/Filme.js
@@ -0,0 +1,33 @@
+const sequelize= require('sequelize');
+const { DataTypes } = require('sequelize');
+
+const cols = {
+    id: {
+        type: DataTypes.INTEGER(10),
+        primaryKey: true,
+        autoIncrement: true
+    },
+    title: {
+        type: DataTypes.STRING(500)
+    },
+    rating: {
+        type: DataTypes.DECIMAL(3,1)
+    },
+    awards: {
+        type: DataTypes.INTEGER(10),
+        default: 0
+    },
+    release_date:{
+        type: DataTypes.DATE
+    },
+    length: {
+        type: DataTypes.INTEGER(10)
+    }
+}
+
+const Filme = sequelize.define('Filme', cols, {
+    tableName: 'Movies',
+    timestamps: false
+});
+
+return Filme;
