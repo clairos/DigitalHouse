48329d2d82230c084a36f23aeeec9bf79953258c
Ana Clara Brusamarello Barbosa <clarasgore@gmail.com>
2022-09-05 11:19:04 -0300

    add dom

---
+++ m8 - javascript front/manipulação de dados/exemplo-script-DOM.js
@@ -0,0 +1,10 @@
+let mainTitle = document.querySelector('h2'); // seleciona a primeira tag h2
+
+mainTitle.innerHTML = 'Primeiro Título' // modifica o conteudo da tag
+
+mainTitle.style.color = 'red' // modifica o css
+mainTitle.style.fontSize = '26px'
+mainTitle.style.fontFamily = 'Arial' 
+
+mainTitle.style.cssText = 'color: red; font-size: 26px; font-family: Arial' // mesma coisa que as linhas de cima porem com menos linhas de codigo
+
