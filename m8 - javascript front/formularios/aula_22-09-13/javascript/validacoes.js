f502c72aef05cbe401c0a80c59ece4a2bf7ddb75
Ana Clara Brusamarello Barbosa <clarasgore@gmail.com>
2022-09-13 11:08:59 -0300

    finalizado

--- m8 - javascript front/formularios/aula_22-09-13/javascript/validacoes.js
+++ m8 - javascript front/formularios/aula_22-09-13/javascript/validacoes.js
@@ -1,5 +1,5 @@
 // seleciona o form
-const form = document.querySelector('form-auth');
+const form = document.querySelector('.form-auth');
 
 // seleciona os inputs
 const emailInput = document.querySelector('input[name="email"]');
@@ -8,44 +8,61 @@ const surnameInput = document.querySelector('input[name="surname"]');
 const userInput = document.querySelector('input[name="username"]');
 const passwordInput = document.querySelector('input[name="password"]');
 
+let erros = [];
+// const erros = [];
 
-form.addEventListener('submit', (event) => {
+form.addEventListener('submit', function(event) { // não usar arrow function!!!
+    erros = [];
+    // erros.length = 0;
+    
+    document.querySelectorAll('small.erro').forEach(erro => {
+        erro.innerHTML = '';
+    })
+    
     event.preventDefault();
 
     // verifica se o value esta preenchido
     if(emailInput.value === ""){
-        alert('Preencha o e-mail');
-        emailInput.focus();
-        return // faz a funcao parar se cair aqui, assim nao roda a funcao inteira
+        erros.push({ name: emailInput.name, message: 'Preencha o e-mail' });
+        emailInput.focus(); // faz a funcao parar se cair aqui, assim nao roda a funcao inteira
     }
 
     if(emailInput.value.includes('@')){
-        alert('Informe um e-mail válido');
+        erros.push({ name: emailInput.name, message: 'Informe um e-mail válido' });
         emailInput.focus();
-        return
     }
 
     if(nameInput.value === ""){
-        alert('Preencha o nome');
+        erros.push({ name: nameInput.name, message: 'Preencha o nome' });
         nameInput.focus();
-        return
     }
 
     if(surnameInput.value === ""){
-        alert('Preencha o sobrenome');
+        erros.push({ name: surnameInput.name, message: 'Preencha o sobrenome' });
         surnameInput.focus();
-        return
+    }
+
+    if(userInput.value === ""){
+        erros.push({ name: userInput.name, message: 'Preencha o usuário' });
+        userInput.focus();
     }
 
     if(passwordInput.value === ""){
-        alert('Preencha a senha');
+        erros.push({ name: passwordInput.name, message: 'Preencha a senha' });
         passwordInput.focus();
-        return
     }
 
-    if(passwordInput.length > 8){
-        alert('A senha deve conter no mínimo 8 caracteres');
+    if(passwordInput.value.length > 8){
+        erros.push({ name: passwordInput.name, message: 'A senha deve conter no mínimo 8 caracteres' });
         passwordInput.focus();
-        return
     }
+
+    // o forEach percorre todo o array
+    // o for normal i=0; i<5; i++, tem controle melhor da iteração
+
+    // o forEach tem um callback que recebe o item atual da iteração
+    // seria a mesma coisa que erros[i]
+    erros.forEach(erro => {
+        document.getElementById(erro.name).innerHTML = erro.message;
+    })
 });
